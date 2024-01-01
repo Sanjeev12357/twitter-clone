80283eab76ac9eabc2a8da0aff58a83a764e7c59
sanjeev saini <sanjeevsinghsaini48@gmail.com>
2024-01-01 19:19:45 +0530

    Second commit

---
+++ components/FeedCard/index.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const FeedCard:React.FC=()=>{
+    return <div className="border">
+        <div className="grid grid-cols-12">
+            <div className="col-span-4"></div>
+            <div className="col-span-8"></div>
+        </div>
+    </div>
+}
+
+export default FeedCard;
