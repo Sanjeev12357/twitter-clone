ebf7f64424dfafea49efff73e758be9c104f6c5e
sanjeev saini <sanjeevsinghsaini48@gmail.com>
2024-01-01 20:08:49 +0530

    added basic home  page layout

--- components/FeedCard/index.tsx
+++ components/FeedCard/index.tsx
@@ -1,10 +1,44 @@
+import Image from "next/image";
 import React from "react";
+import { FaRetweet } from "react-icons/fa"
+import { BiMessageRounded, BiUpload } from "react-icons/bi";
+import { AiOutlineHeart } from "react-icons/ai";
 
 const FeedCard:React.FC=()=>{
-    return <div className="border">
-        <div className="grid grid-cols-12">
-            <div className="col-span-4"></div>
-            <div className="col-span-8"></div>
+    return <div className="border border-r-0 border-b-0 border-l-0 border-gray-600 p-5 hover:bg-slate-900 transition-all cursor-pointer">
+        <div className="grid grid-cols-12 gap-3">
+            <div className="col-span-1">
+                <Image
+                src="https://avatars.githubusercontent.com/u/124911392?v=4"
+                alt="user-image"
+                height={50}
+                width={50}
+                />
+            </div>
+            <div className="col-span-11">
+                <h5>
+                    Sanjeev Singh
+                </h5>
+                <p>
+                    Is it just me or everyone else? Do you feel the code quality 
+                    decrease as the project size increases? Just refactored a lot of bad code #codinglife
+                </p>
+                <div className="flex justify-between mt-5  text-xl items-center p-2 pr-10 w-[90%]">
+                    <div>
+                    <BiMessageRounded/>
+                    </div>
+                    <div>
+                        <FaRetweet/>
+                    </div>
+                    <div>
+                        <AiOutlineHeart/>
+                    </div>
+                    <div>
+                        <BiUpload/>
+                    </div>
+                     
+                </div>
+            </div>
         </div>
     </div>
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,7 @@
 import Image from "next/image";
 import {BsBell, BsBookmark, BsEnvelope, BsTwitter } from 'react-icons/bs'
-import {BiHash, BiHomeCircle, BiUser} from 'react-icons/bi'
+import {BiHash, BiHomeCircle, BiMoney, BiUser} from 'react-icons/bi'
+import { SlOptions } from "react-icons/sl"
 import { Inter } from "next/font/google";
 import React from "react";
 import FeedCard from "@/components/FeedCard";
@@ -32,34 +33,48 @@ const SidebarMenuItems:TwitterSidebarButton[]=[
     icon:<BsBookmark/>
   },
   {
+    title:'twitter blu',
+    icon:<BiMoney/>
+  },
+  {
+    title:"More options",
+    icon:<SlOptions/>
+  },
+  {
     title:'Profile',
     icon:<BiUser/>
-  }
+  },
+ 
 ]
 export default function Home() {
   return (
-    <div className={inter.className}>
+    <div >
       <div className="grid grid-cols-12 h-screen w-screen px-56">
-        <div className="col-span-3  pt-8  ">
-          <div className="text-4xl w-fit h-fit hover:bg-gray-600  rounded-full p-2 cursor-pointer transition-all ">
+        <div className="col-span-3  pt-1 ml-28 ">
+          <div className="text-2xl w-fit h-fit hover:bg-gray-600  rounded-full p-2 cursor-pointer transition-all ">
           <BsTwitter className=""/>
           </div>
-          <div className="mt-4 text-2xl  pr-4">
+          <div className="mt-1 text-xl  pr-4">
             <ul>
               {
-                SidebarMenuItems.map(item=><li className="flex mt-2 justify-start items-center gap-4 hover:bg-gray-800 rounded-full px-5 py-2 w-fit cursor-pointer " key={item.title}><span>{item.icon}</span> <span>{item.title}</span></li>)
+                SidebarMenuItems.map(item=><li className="flex mt-2 justify-start items-center gap-4 hover:bg-gray-800 rounded-full px-3 py-3 w-fit cursor-pointer " key={item.title}><span className="text-3xl">{item.icon}</span> <span>{item.title}</span></li>)
               }
             </ul>
             <div className="mt-4 px-3">
-            <button className="bg-[#1d9bf0] font-semibold text-lg p-4 rounded-full w-full ">Tweet</button>
+            <button className="bg-[#1d9bf0] font-semibold text-lg py-2 px-4 rounded-full w-full ">Tweet</button>
             </div>
             
           </div>
 
          
         </div>
-        <div className="col-span-6 border-r-[1px] border-l-[1px] border-gray-400">
+        <div className="col-span-5 border-r-[1px] border-l-[1px] border border-gray-600">
 
+              <FeedCard/>
+              <FeedCard/>
+              <FeedCard/>
+              <FeedCard/>
+              <FeedCard/>
               <FeedCard/>
 
         </div>
