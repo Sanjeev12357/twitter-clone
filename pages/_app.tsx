dba5c833690d588baf1c28baa7fe199a293ac348
sanjeev saini <sanjeevsinghsaini48@gmail.com>
2024-01-02 16:01:14 +0530

    added authentication flow

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,17 +3,25 @@ import type { AppProps } from 'next/app'
 import { Inter,Quicksand } from "next/font/google";
 import { GoogleOAuthProvider } from '@react-oauth/google';
 import { Toaster } from 'react-hot-toast';
+import { QueryClientProvider,QueryClient } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+
 const inter = Inter({ subsets: ["latin"] });
 
-
+const querClient=new QueryClient();
 export default function App({ Component, pageProps }: AppProps) {
   return (
   <div className={inter.className}>
+    <QueryClientProvider client={querClient}>
+
+   
     <GoogleOAuthProvider clientId="17351229008-tc64v5vmqj1cb0516bs791k5f9gsqmm5.apps.googleusercontent.com">
     <Component {...pageProps} />
     <Toaster/>
+    <ReactQueryDevtools/>
     </GoogleOAuthProvider>
-   
+    
+    </QueryClientProvider>
   </div>
 
   )
--- pages/index.tsx
+++ pages/index.tsx
@@ -9,6 +9,8 @@ import { CredentialResponse, GoogleLogin } from "@react-oauth/google";
 import toast from "react-hot-toast";
 import { graphqlClient } from "@/clients/api";
 import { verifyUserGoogleTokenQuery } from "@/graphql/query/user";
+import {useCurrentUser} from "../hooks/user";
+import { useQueryClient } from "@tanstack/react-query";
 const inter = Inter({ subsets: ["latin"] });
 
 interface TwitterSidebarButton{
@@ -51,6 +53,9 @@ const SidebarMenuItems:TwitterSidebarButton[]=[
  
 ]
 export default function Home() {
+  const {user} =useCurrentUser();
+   const queryClient=useQueryClient();
+  console.log(user);
   const handleLoginWithGoogle=useCallback(async(cred:CredentialResponse)=>{
     const googleToken=cred.credential
     if(!googleToken){
@@ -61,12 +66,13 @@ export default function Home() {
     toast.success('verified success');
     console.log(verifyGoogleToken);
     if(verifyGoogleToken) window.localStorage.setItem('token',verifyGoogleToken);
-
-  },[])
+     
+    await queryClient.invalidateQueries(["curent-user"]);
+  },[queryClient])
   return (
     <div >
       <div className="grid grid-cols-12 h-screen w-screen px-56">
-        <div className="col-span-3  pt-1 ml-28 ">
+        <div className="col-span-3 relative  pt-1 ml-28 ">
           <div className="text-2xl w-fit h-fit hover:bg-gray-600  rounded-full p-2 cursor-pointer transition-all ">
           <BsTwitter className=""/>
           </div>
@@ -79,8 +85,18 @@ export default function Home() {
             <div className="mt-4 px-3">
             <button className="bg-[#1d9bf0] font-semibold text-lg py-2 px-4 rounded-full w-full ">Tweet</button>
             </div>
+
             
           </div>
+          {
+            user &&           <div className=" absolute bottom-5 flex gap-2 bg-slate-800 p-3 rounded-full px-3 py-2 ">
+            { user && user.profileImageURL && <Image className="rounded-full" src={user?.profileImageURL} alt="user-image" height={50} width={50} />}
+            <h3 className="text-xl">{user?.firstName}</h3>
+          </div>
+          }
+
+
+          
 
          
         </div>
@@ -95,10 +111,13 @@ export default function Home() {
 
         </div>
         <div className="col-span-3 p-5">
-          <div className="p-5 bg-slate-700 rounded-lg  ">
+          {
+            !user && <div className="p-5 bg-slate-700 rounded-lg  ">
             <h1 className="my-2 text-2xl" >New to twitter?</h1>
           <GoogleLogin onSuccess={handleLoginWithGoogle}/>
           </div>
+          }
+          
         </div>
       </div>
     </div>
